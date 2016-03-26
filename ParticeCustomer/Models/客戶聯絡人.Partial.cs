5782f3c8a680a4c7b4bf792a9e39002f254bb61c
YafanXue <yafan@edetw.com>
2016-03-26 14:53:08 +0800

    bug fix

--- ParticeCustomer/Models/客戶聯絡人.Partial.cs
+++ ParticeCustomer/Models/客戶聯絡人.Partial.cs
@@ -3,6 +3,7 @@ namespace ParticeCustomer.Models
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
+    using System.Linq;
     using System.Text.RegularExpressions;
     using System.Web.Mvc;
     [MetadataType(typeof(客戶聯絡人MetaData))]
@@ -16,7 +17,14 @@ namespace ParticeCustomer.Models
 
             if (!regex.IsMatch(this.手機))
                 yield return new ValidationResult("手機格式錯誤,電話格式必須為( e.g. 0911-111111 )", new[] { "手機" });
-         
+            客戶資料Entities db = new 客戶資料Entities();
+
+            var data = db.客戶聯絡人.FirstOrDefault(p => p.Email == this.Email && p.客戶Id == this.客戶Id && p.Id != this.Id);
+            if (data != null)
+                yield return new ValidationResult("Email帳號重複", new[] { "Email" });
+
+
+
         }
     }
 
@@ -35,7 +43,6 @@ namespace ParticeCustomer.Models
         [Required]
         public string 姓名 { get; set; }
 
-        [MailNotExists]
         [EmailAddress(ErrorMessage = "Email格式錯誤")]
         [StringLength(250, ErrorMessage="欄位長度不得大於 250 個字元")]
         [Required]
